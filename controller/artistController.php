dc07d8596ce012a9bdb594e622e9dba86b4e9e12
IgnacioChiaradia <39503621+IgnacioChiaradia@users.noreply.github.com>
2018-11-03 00:16:08 -0300

    Agrego codigo a conncection para que detecte errores y agrego metodos en artistController

--- controller/artistController.php
+++ controller/artistController.php
@@ -67,6 +67,57 @@ class ArtistController extends Controller{
 
 	}
 
+	    public function delete($id)
+    {
+        $searchedArtist = $this->artistDao->delete($id);
+        $this->list(); // reutilizo el list()
+    }
+
+
+    public function update($nombre,$id_artist)
+    {    	
+    	$artist = new Artist($nombre);
+    	$artist->setIdArtist($id_artist);
+    	
+    	// se muestra que se modifico correctamente el artista
+    		$mensaje['mensaje'] = "EL ARTISTA SE MODIFICO CON EXITO !";
+    		$mensaje['tipo'] = "success";
+
+    	try
+    	{
+    		$this->artistDao->update($artist);    		
+    	}
+    	catch(\PDOException $e)
+    	{
+    		$mensaje['mensaje'] = "UPS! ERROR PDO: " . $e->getMessage() . "| CODE: " . $e->getCode();
+	    	$mensaje['tipo'] = "danger";
+    	}
+    	catch(\Exception $e){
+	    	$mensaje['mensaje'] = "UPS! ERROR EXCEPTION: " . $e->getMessage() . "| CODE: " . $e->getCode();
+	    	$mensaje['tipo'] = "danger";
+	    }
+    	
+
+    	
+
+		$searchedArtist = $this->artistDao->read($id_artist); // artista buscado
+
+
+		include URL_VIEW . 'header.php';
+	    require(URL_VIEW . "viewArtist/updateArtist.php");
+	    include URL_VIEW . 'footer.php';    	
+
+    }
+
+    public function updateView($id) 
+    {
+    	$searchedArtist = $this->artistDao->read($id); // artista buscado
+
+
+    	include URL_VIEW . 'header.php';
+	    require(URL_VIEW . "viewArtist/updateArtist.php");
+	    include URL_VIEW . 'footer.php';
+    }
 
 
 }
--- dao/connection.php
+++ dao/connection.php
@@ -5,8 +5,9 @@
         public function __construct(){}
 
         public static function connect() {
+            /*con array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION) habilitas al PDO que notifique siempre que ocurra una excepcion en las consultas SQL*/
             try{
-                return new \PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASS);
+                return new \PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASS, array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
             }catch(PDOException $e){
                 echo 'Error '. $e->getMessage();
                 die();
