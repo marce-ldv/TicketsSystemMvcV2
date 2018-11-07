32de61c4a6e8d76c5abd7e7c8f05ae9d274c24a1
IgnacioChiaradia <39503621+IgnacioChiaradia@users.noreply.github.com>
2018-11-07 15:34:54 -0300

    Correcion de metodo update en daos, creacion de typeAreaDAO,y model typeArea

---
+++ model/typeArea.php
@@ -0,0 +1,43 @@
+<?php
+/*typeAreas(tipo plazas)*/
+namespace model;
+
+class ClassName extends AnotherClass
+{
+    private $idTypeArea;
+    private $descriptionTypeArea;
+
+    function __construct($idTypeArea, $descriptionTypeArea)
+    {
+      $this->$idTypeArea = $idTypeArea;
+      $this->$descriptionTypeArea = $descriptionTypeArea;
+    }
+
+// getters
+
+    public function getIdTypeArea()
+    {
+        return $this->idTypeArea;
+    }
+
+    public function getDescriptionTypeArea()
+    {
+        return $this->descriptionTypeArea;
+    }
+
+//setters
+
+    public function setIdTypeArea($idTypeArea)
+    {
+        $this->idTypeArea = $idTypeArea;
+
+        return $this;
+    }
+
+    public function setDescriptionTypeArea($descriptionTypeArea)
+    {
+        $this->descriptionTypeArea = $descriptionTypeArea;
+
+        return $this;
+    }
+}
