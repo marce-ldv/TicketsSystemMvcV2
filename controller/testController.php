b150d9e70ccf3b5bd57cd0d30bb05fc855bd3bec
Sergio <molinasergio91@gmail.com>
2018-11-07 16:55:35 -0300

    test

--- controller/testController.php
+++ controller/testController.php
@@ -5,20 +5,46 @@ use controller\Controller as Controller;
 use model\User as User;
 use model\Category as Category;
 use helpers\ConverterCase as ConverterCase;
+use dao\DefaultDAO as DefaultDAO;
 
 class TestController extends Controller{
 
   public function index(){
+    $this->render("test/home");
+  }
 
-    $category = new Category();
+  public function register ($registerData = []) {
 
-    $category->setDescription("Segio es groso :D:D:D:D:D:D");
-    $category->setIdCategory(null);
+    if ($this->isMethod("POST")) $this->redirect("/test/");
+    if (empty($registerData)) $this->redirect("/test/"); 
 
-    $repository = $this->defaultDAO->getRepository(Category::class);
+    $user = new User();
+    $repository = $this->defaultDAO->getRepository(User::class);
+    $criteria = [
+      "username" => $registerData["username"],
+      "email" => $registerData["email"]
+    ];
 
-    $repository->create($category);
+    if ($repository->findOneBy($criteria, "OR")) {
+       $this->redirect("/test/", ["alert" => "El usuario o email ya estan registrados"]);
+     }
 
-    $this->render('home');
+    if ($registerData["pass"] != $registerData["passAgain"]) {
+      $this->redirect("/test/", ["alert" => "Las contraseñas no coinciden"]);
+     }
+
+    $hash = password_hash($registerData["pass"],PASSWORD_DEFAULT);
+
+    $user->setUsername($registerData["username"])
+      ->setPass($hash)
+      ->setEmail($registerData["email"])
+      ->setRoleUser("user")
+      ->setNameUser($registerData["name_user"])
+      ->setSurname($registerData["surname"])
+      ->setDni($registerData["dni"]);
+
+    $repository->create($user);
+
+    $this->redirect ('/test/', ["alert" => "Usuario creado con exito"]);
   }
 }
