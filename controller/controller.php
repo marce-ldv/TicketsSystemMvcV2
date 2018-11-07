5b91375f081bfa1cc3244075da986dea68f6d705
Sergio <molinasergio91@gmail.com>
2018-11-07 16:55:10 -0300

    Arreglos en controller y repository

--- controller/controller.php
+++ controller/controller.php
@@ -18,7 +18,7 @@ class Controller{
 
   public function getToken(){
     if( ! isset($this->session->token))
-      return false;
+    return false;
 
     $user = new User();
     $user->unserialize($this->session->token);
@@ -29,21 +29,39 @@ class Controller{
     return ($this->getToken() ? true : false);
   }
 
-  public function redirect($url){
+  public function redirect($url, $options = []){
+    //serialize options
+    if (! empty($options)) {
+      $optionsSerialize = serialize($options);
+      $this->session->redirectOptions = $optionsSerialize;
+    }
+    //redirect
     header("location: ". VIEW_URL . $url);
   }
 
-  public function render($path, $options = "") {
-        if ( ! empty($options)) {
-            forEach($options as $key => $value) {
-                ${$key} = $value;
-            }
-        }
+  public function render($path, $options = []) {
 
-        include URL_VIEW . 'header.php';
-        require(URL_VIEW . "$path" . ".php");
-        include URL_VIEW . 'footer.php';
+    //Revisa la session por options de redirect
+    if ( $this->session->redirectOptions ) {
+      $optionsUnserialize = unserialize($this->session->redirectOptions);
+      unset($this->session->redirectOptions);
+      $options = array_merge($optionsUnserialize, $options);
     }
+
+    if ( ! empty($options)) {
+      forEach($options as $key => $value) {
+        ${$key} = $value;
+      }
+    }
+
+    include URL_VIEW . 'header.php';
+    require(URL_VIEW . "$path" . ".php");
+    include URL_VIEW . 'footer.php';
+  }
+
+  public function isMethod ($method) {
+    return $_SERVER["REQUEST_METHOD"] == $method;
+  }
 
 
 }
