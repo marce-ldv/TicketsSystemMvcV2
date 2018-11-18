0af6c3ad94a1a7248b1d2ec87bbd789c82914727
Marcelo Sanchez <marcelo.docutec@gmail.com>
2018-11-18 12:46:56 -0800

    dashboard

--- view/dashboard.php
+++ view/dashboard.php
@@ -81,22 +81,46 @@
     </div>
     <div class="col-8">
       <div class="tab-content" id="nav-tabContent">
+
         <div class="tab-pane fade show active" id="list-artist" role="tabpanel" aria-labelledby="list-home-list">
           <h2><a href="<?= FRONT_VIEW ?>/artist/">Artistas</a></h2>
+          <p>crud de artistas</p>
         </div>
+
         <div class="tab-pane fade" id="list-category" role="tabpanel" aria-labelledby="list-profile-list">
           <h2><a href="<?= FRONT_VIEW ?>/category/">Categorias</a></h2>
+          <p>crud de categorias</p>
         </div>
+
         <div class="tab-pane fade" id="list-type-area" role="tabpanel" aria-labelledby="list-settings-list">
-          <h2><a href="<?= FRONT_VIEW ?>/typeArea/">Tipo de area(tipo plaza) palco,platea</a></h2>
+          <h2><a href="<?= FRONT_VIEW ?>/typeArea/">Tipo de area(tipo plaza)</a></h2>
+          <p>palco,platea, popular, etc</p>
         </div>
-         <div class="tab-pane fade" id="list-event" role="tabpanel" aria-labelledby="list-messages-list"><h2><a href="<?= FRONT_VIEW ?>/event/">Eventos</a></h2></div>
-         <div class="tab-pane fade" id="list-place-event" role="tabpanel" aria-labelledby="list-label-place-event"><h2><a href="<?= FRONT_VIEW ?>/placeEvent/">Tipo de place event(Lugar de evento) capacidad 95000 personas, Estadio libertadores de america por ejemplo</a></h2></div>
-         <div class="tab-pane fade" id="list-calendar" role="tabpanel" aria-labelledby="list-calendar-label"><h2><a href="<?= FRONT_VIEW ?>/calendar/">Calendario</a></h2></div>
-      <!-- <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list"><h2><a href="<?= FRONT_VIEW ?>/purchase/">Compras</a></h2></div>
+
+         <div class="tab-pane fade" id="list-event" role="tabpanel" aria-labelledby="list-messages-list">
+         <h2><a href="<?= FRONT_VIEW ?>/event/">Eventos</a></h2>
+         <p>crud de eventos</p>
+         </div>
+
+         <div class="tab-pane fade" id="list-place-event" role="tabpanel" aria-labelledby="list-label-place-event">
+         <h2><a href="<?= FRONT_VIEW ?>/placeEvent/">Tipo de place event(Lugar de evento)</a></h2>
+         <p>Capacidad 95000 personas, Estadio libertadores de america por ejemplo</p>
+         </div>
+
+         <div class="tab-pane fade" id="list-calendar" role="tabpanel" aria-labelledby="list-calendar-label">
+         <h2><a href="<?= FRONT_VIEW ?>/calendar/">Calendario</a></h2>
+         <p>crud de calendario</p>
+         </div>
+      <!-- <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+      <h2><a href="<?= FRONT_VIEW ?>/purchase/">Compras</a></h2>
+      <p>crud de compras</p>
+      </div>
 
 
-      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list"><h2><a href="<?= FRONT_VIEW ?>/eventArea/">Tipo de event area(asientos)</a></h2></div>
+      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
+      <h2><a href="<?= FRONT_VIEW ?>/eventArea/">Tipo de event area()</a></h2>
+      <p>crud de type event area</p>
+      </div>
 
       <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>-->
 
