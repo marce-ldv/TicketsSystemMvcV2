77b7e2fc85d89fb2416b64a89df27c46d8a616c4
Marcelo Sanchez <marcelo.docutec@gmail.com>
2018-11-18 12:38:36 -0800

    Calendar Crud, views, dashboard

--- model/calendar.php
+++ model/calendar.php
@@ -10,7 +10,7 @@ class Calendar
   private $dateStart;
   private $dateEnd;
 
-  public function __construct($idCalendar="", $event="", $placeEvent="", $dateEnd="", $dateStart=""){
+  public function __construct($idCalendar="", $event="", $placeEvent="", $dateStart="", $dateEnd=""){
     $this->idCalendar = $idCalendar;
     $this->event = $event;
     $this->placeEvent = $placeEvent;
@@ -18,91 +18,52 @@ class Calendar
     $this->dateStart = $dateStart;
   }
 
-    /**
-   * @return mixed
-   */
   public function getIdCalendar()
   {
       return $this->idCalendar;
   }
 
-  /**
-   * @param mixed $idCalendar
-   *
-   * @return self
-   */
+
   public function setIdCalendar($idCalendar) :void
   {
       $this->idCalendar = $idCalendar;
   }
 
-  /**
-   * @return mixed
-   */
   public function getEvent()
   {
       return $this->event;
   }
 
-  /**
-   * @param mixed $event
-   *
-   * @return self
-   */
   public function setEvent($event) :void
   {
       $this->event = $event;
   }
 
-  /**
-   * @return mixed
-   */
   public function getPlaceEvent()
   {
       return $this->placeEvent;
   }
 
-  /**
-   * @param mixed $placeEvent
-   *
-   * @return self
-   */
   public function setPlaceEvent($placeEvent) :void
   {
       $this->placeEvent = $placeEvent;
   }
 
-  /**
-   * @return mixed
-   */
   public function getDateStart()
   {
       return $this->dateStart;
   }
 
-  /**
-   * @param mixed $dateStart
-   *
-   * @return self
-   */
   public function setDateStart($dateStart) :void
   {
       $this->dateStart = $dateStart;
   }
 
-  /**
-   * @return mixed
-   */
   public function getDateEnd()
   {
       return $this->dateEnd;
   }
 
-  /**
-   * @param mixed $dateEnd
-   *
-   * @return self
-   */
   public function setDateEnd($dateEnd)
   {
       $this->dateEnd = $dateEnd;
