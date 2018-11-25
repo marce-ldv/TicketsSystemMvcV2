75c5bea5d6ed870f2ff38c4fcc70097309ebac87
marcelo-docutec <marcelo.docutec@gmail.com>
2018-11-25 19:11:36 -0300

    connection, ExcNonQuery

--- dao/connection.php
+++ dao/connection.php
@@ -1,17 +1,87 @@
 <?php namespace dao;
 
-    class Connection {
+/**
+*
+*/
+class Connection {
 
-        public function __construct(){}
+     private $pdo = null;
+     private $pdoStatement = null;
+     private static $instance = null;
 
-        public static function connect() {
-            /*con array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION) habilitas al PDO que notifique siempre que ocurra una excepcion en las consultas SQL*/
-            try{
-                return new \PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASS, array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
-            }catch(PDOException $e){
-                echo 'Error '. $e->getMessage();
-                die();
-            }
-        }
-    }
-?>
+     /**
+      *
+      */
+     public function __construct() {
+          try {
+               $this->pdo = new \PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+               $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+          } catch (Exception $ex) {
+               throw $ex;
+          }
+     }
+
+     /**
+      *
+      */
+     public static function getInstance()
+     {
+         if(self::$instance == null)
+            self::$instance = new Connection();
+
+         return self::$instance;
+     }
+
+
+     /**
+      *
+      */
+     public function execute($query, $parameters = array())
+     {
+          try
+          {
+			// Creo una sentencia llamando a prepare. Esto devuelve un objeto statement
+               $this->pdoStatement = $this->pdo->prepare($query);
+
+               foreach($parameters as $parameterName => $value)
+               {
+                    // Reemplazo los marcadores de parametro por los valores reales utilizando el método bindParam().
+                    $this->pdoStatement->bindParam(":".$parameterName, $value);
+               }
+
+               $this->pdoStatement->execute();
+
+               return $this->pdoStatement->fetchAll();
+          }
+          catch(Exception $ex)
+          {
+               throw $ex;
+          }
+     }
+
+     /**
+      *
+      */
+     public function executeNonQuery($query, $parameters = array())
+     {
+          try
+          {
+               // Creo una sentencia llamando a prepare. Esto devuelve un objeto statement
+               $this->pdoStatement = $this->pdo->prepare($query);
+
+               foreach($parameters as $parameterName => $value) {
+                    // Reemplazo los marcadores de parametro por los valores reales utilizando el método bindParam().
+                    $this->pdoStatement->bindParam(":$parameterName", $parameters[$parameterName]);
+               }
+
+               $this->pdoStatement->execute();
+
+               return $this->pdoStatement->rowCount();
+          }
+          catch(\PDOException $ex)
+          {
+               throw $ex;
+          }
+     }
+
+}
