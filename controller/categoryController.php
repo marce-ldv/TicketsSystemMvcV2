0de368a9e8c176aa7c759743c807622548bf4da9
sanson222 <molinasergio91@gmail.com>
2018-11-26 19:14:12 -0300

    mejorando codigo

--- controller/categoryController.php
+++ controller/categoryController.php
@@ -1,103 +1,114 @@
 <?php
-
-namespace controller;
-
-use model\Category as Category;
-use dao\CategoryDAO as CategoryDAO;
-use controller\Controller as Controller;
-use interfaces\IAlmr as IAlmr;
-
-class CategoryController extends Controller implements IAlmr
-{
-  private $controllerDao;
-
-  function __construct()
-  {
-    parent::__construct();
-    $this->controllerDao = CategoryDAO::getInstance();
-  }
-
-  public function index()
-  {
-    $this->list();
-  }
-
-  public function add ($data = []) {
-    //create -> La llave es el campo en la base de dato y el valor es el valor a guardar en la base de dato
-
-    $category = new Category(
-      '',
-      $data["name_category"]
-    );
-
-    $this->controllerDao->create($category);
-
-    $this->redirect("/category/");
-
-    return;
-  }
-
-  public function list () {
-    //if ( ! $this->isLogged()) {
-      //$this->redirect('/default/login');
-    //}
-    //else {
-
-      $items = $this->controllerDao->readAll();
-
-      if ($items) {
-        $items = (! is_array($items)) ? [$items] : $items;
-      }else {
-        $items = [];
-      }
-
-      //$items = $this->controllerDao->mapMethod($items);
-
-      $this->render("viewCategory/categories",[
-        'items' => $items
-      ]);
-    //}
-  }
-
-  public function remove($data = []) {
-
-    $this->controllerDao->delete($data['id']);
-
-    $this->index();
-  }
-
-  public function viewEdit ($id) {
-
-    $searchedItem = $this->controllerDao->read($id);
-
-    $this->render('viewCategory/updateCategory',[
-      'searchedItem' => $searchedItem
-    ]);
-  }
-
-  public function modify($data = [])
-  {
-    if ( ! $this->isMethod("POST")) $this->redirect("/default/");
-    if (empty($data)) $this->redirect("/default/");
-
-    $category = new Category(
-      $data["id"],
-      $data["name_category"]
-    );
-
-    try
+    
+    namespace controller;
+    
+    use model\Category as Category;
+    use dao\CategoryDAO as CategoryDAO;
+    use controller\Controller as Controller;
+    use interfaces\IAlmr as IAlmr;
+    
+    class CategoryController extends Controller implements IAlmr
     {
-      $this->controllerDao->update($category);
+        private $controllerDao;
+        
+        function __construct () {
+            parent::__construct();
+            $this->controllerDao = CategoryDAO::getInstance();
+        }
+    
+        /**
+         *
+         */
+        public function index () {
+            $this->list();
+        }
+    
+        /**
+         * @param array $data
+         */
+        public function add ($data = []) {
+            //create -> La llave es el campo en la base de dato y el valor es el valor a guardar en la base de dato
+            
+            $category = new Category(
+                '',
+                $data["name_category"]
+            );
+            
+            $this->controllerDao->create($category);
+            
+            $this->redirect("/category/");
+            
+            return;
+        }
+    
+        /**
+         *
+         */
+        public function list () {
+            //if ( ! $this->isLogged()) {
+            //$this->redirect('/default/login');
+            //}
+            //else {
+            
+            $items = $this->controllerDao->readAll();
+            
+            if ($items) {
+                $items = (!is_array($items)) ? [$items] : $items;
+            } else {
+                $items = [];
+            }
+            
+            //$items = $this->controllerDao->mapMethod($items);
+            
+            $this->render("viewCategory/categories", [
+                'items' => $items
+            ]);
+            //}
+        }
+    
+        /**
+         * @param array $data
+         */
+        public function remove ($data = []) {
+            
+            $this->controllerDao->delete($data['id']);
+            
+            $this->index();
+        }
+    
+        /**
+         * @param $id
+         */
+        public function viewEdit ($id) {
+            
+            $searchedItem = $this->controllerDao->read($id);
+            
+            $this->render('viewCategory/updateCategory', [
+                'searchedItem' => $searchedItem
+            ]);
+        }
+    
+        /**
+         * @param array $data
+         */
+        public function modify ($data = []) {
+            if (!$this->isMethod("POST")) $this->redirect("/default/");
+            if (empty($data)) $this->redirect("/default/");
+            
+            $category = new Category(
+                $data["id"],
+                $data["name_category"]
+            );
+            
+            try {
+                $this->controllerDao->update($category);
+            } catch (\PDOException $e) {
+                echo $e->getMessage();
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+            }
+            
+            $this->index();
+            
+        }
     }
-    catch(\PDOException $e)
-    {
-      echo $e->getMessage();
-    }
-    catch(\Exception $e){
-      echo $e->getMessage();
-    }
-
-    $this->index();
-
-  }
-}
--- dao/categoryDAO.php
+++ dao/categoryDAO.php
@@ -1,122 +1,140 @@
 <?php
-
-namespace dao;
-
-use model\Category as Category;
-use interfaces\ICrud as ICrud;
-use helpers\Collection as Collection;
-use dao\Singleton as Singleton;
-
-class CategoryDAO extends Singleton implements ICrud
-{
-	private $connection;
-
-	public function __construct()	{}
-
-		public function create($_data)
-		{
-			try {
-
-				$sql = "INSERT INTO categories (name_category) VALUES (:nameCategory)";
-
-				$parameters['nameCategory'] = $_data->getNameCategory();
-
-				// creo la instancia connection
-				$this->connection = Connection::getInstance();
-				// Ejecuto la sentencia.
-				return $this->connection->executeNonQuery($sql, $parameters);
-			}
-			catch(PDOException $ex)
-			{
-				throw $ex;
-			}
-		}
-
-		public function read($id)
-		{
-			try {
-
-				$sql = "SELECT * FROM categories where id_category = :id";
-
-				$parameters['id'] = $id;
-
-				$this->connection = Connection::getInstance();
-				$resultSet = $this->connection->execute($sql, $parameters);
-			} catch(Exception $ex) {
-				throw $ex;
-			}
-
-
-			if(!empty($resultSet))
-			return $this->mapMethod($resultSet);
-			else
-			return false;
-		}
-
-		public function readAll()
-		{
-			try {
-
-				$sql = "SELECT * FROM categories";
-
-				$this->connection = Connection::getInstance();
-				$resultSet = $this->connection->execute($sql);
-			} catch(Exception $ex) {
-				throw $ex;
-			}
-
-			if(!empty($resultSet))
-			return $this->mapMethod($resultSet);
-			else
-			return false;
-		}
-
-		public function update($value)
-		{
-			$sql = "UPDATE categories SET name_category = :name_category WHERE id_category = :id ";
-
-			$parameters['id'] = $value->getIdCategory();
-			$parameters['name_category'] = $value->getNameCategory();
-
-			try {
-
-				$this->connection = Connection::getInstance();
-
-				return $this->connection->executeNonQuery($sql, $parameters);
-			} catch(PDOException $ex) {
-				throw $ex;
-			}
-
-		}
-
-		public function delete($id)
-		{
-
-			try
-			{
-				$sql = "DELETE FROM categories WHERE id_category = :id";
-				$parameters['id'] = $id;
-
-				$this->connection = Connection::getInstance();
-				return $this->connection->ExecuteNonQuery($sql, $parameters);
-
-
-			} catch(PDOException $Exception) {
-
-				throw new MyDatabaseException( $Exception->getMessage() , $Exception->getCode());
-
-			}
-		}
-
-		public function mapMethod($value) {
-
-			$value = is_array($value) ? $value : [$value];
-
-			$resp = array_map(function($p){
-				return new Category($p['id_category'], $p['name_category']);
-			}, $value);
-
-			return count($resp) > 1 ? $resp : $resp[0];
-
-		}
-	}
+    
+    namespace dao;
+    
+    use model\Category as Category;
+    use dao\Singleton as Singleton;
+    use PDOException;
+    use Exception;
+    
+    class CategoryDAO extends Singleton
+    {
+        private $connection;
+        
+        public function __construct () {
+        }
+    
+        /**
+         * @param $_data
+         * @return int
+         */
+        public function create (Category $_data) {
+            try {
+                
+                $sql = "INSERT INTO categories (name_category) VALUES (:nameCategory)";
+                
+                $parameters['nameCategory'] = $_data->getNameCategory();
+                
+                // creo la instancia connection
+                $this->connection = Connection::getInstance();
+                // Ejecuto la sentencia.
+                return $this->connection->executeNonQuery($sql, $parameters);
+            } catch (PDOException $ex) {
+                throw $ex;
+            }
+        }
+    
+        /**
+         * @param $id
+         * @return array|bool
+         * @throws Exception
+         */
+        public function read ($id) {
+            try {
+                
+                $sql = "SELECT * FROM categories where id_category = :id";
+                
+                $parameters['id'] = $id;
+                
+                $this->connection = Connection::getInstance();
+                $resultSet = $this->connection->execute($sql, $parameters);
+            } catch (Exception $ex) {
+                throw $ex;
+            }
+            
+            
+            if (!empty($resultSet))
+                return $this->mapMethod($resultSet);
+            else
+                return false;
+        }
+    
+        /**
+         * @return array|bool
+         * @throws Exception
+         */
+        public function readAll () {
+            try {
+                
+                $sql = "SELECT * FROM categories";
+                
+                $this->connection = Connection::getInstance();
+                $resultSet = $this->connection->execute($sql);
+            } catch (Exception $ex) {
+                throw $ex;
+            }
+            
+            if (!empty($resultSet))
+                return $this->mapMethod($resultSet);
+            else
+                return false;
+        }
+    
+        /**
+         * @param $value Category
+         * @return int
+         */
+        public function update (Category $value) {
+            $sql = "UPDATE categories SET name_category = :name_category WHERE id_category = :id ";
+            
+            $parameters['id'] = $value->getIdCategory();
+            $parameters['name_category'] = $value->getNameCategory();
+            
+            try {
+                
+                $this->connection = Connection::getInstance();
+                
+                return $this->connection->executeNonQuery($sql, $parameters);
+            } catch (PDOException $ex) {
+                throw $ex;
+            }
+            
+        }
+    
+        /**
+         * @param $id
+         * @return int
+         */
+        public function delete ($id) {
+            
+            try {
+                $sql = "DELETE FROM categories WHERE id_category = :id";
+                $parameters['id'] = $id;
+                
+                $this->connection = Connection::getInstance();
+                return $this->connection->ExecuteNonQuery($sql, $parameters);
+                
+                
+            } catch (PDOException $Exception) {
+                
+                throw $Exception;
+                
+            }
+        }
+    
+        /**
+         * @param $value
+         * @return array
+         */
+        public function mapMethod ($value) {
+            
+            $value = is_array($value) ? $value : [$value];
+            
+            $resp = array_map(function ($p) {
+                return new Category($p['id_category'], $p['name_category']);
+            }, $value);
+            
+            return count($resp) > 1 ? $resp : $resp[0];
+            
+        }
+    }
